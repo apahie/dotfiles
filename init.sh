6987e4b6fd4ed93dd533d798ef978887ea332049
apahie <apahie0918@gmail.com>
2023-10-30 02:51:51 +0900

    add webterm config

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 # neovim & nvchad
 ln -s ~/dotfiles/nvchad/custom ~/dotfiles/.config/nvim/lua/
 ln -s ~/dotfiles/.config/nvim ~/.config/
+
+# wezterm
+ln -s ~/dotfiles/.wezterm.lua ~/.wezterm.lua
