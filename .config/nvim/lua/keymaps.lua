652f836cd4305212b8b19cc5d596f9a954a4d88c
nagano-hirofumi-16 <nagano.hirofumi.16@rkkcs.jp>
2026-04-30 20:26:13 +0900

    nvim: keymaps から <leader>w/q を削除

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,10 +4,6 @@ vim.g.maplocalleader = " "
 
 local map = vim.keymap.set
 
--- ファイル操作
-map("n", "<leader>w", "<cmd>w<cr>", { desc = "保存" })
-map("n", "<leader>q", "<cmd>q<cr>", { desc = "終了" })
-
 -- 検索ハイライトをクリア
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "ハイライトをクリア" })
 
@@ -16,3 +12,4 @@ map("n", "<C-h>", "<C-w>h", { desc = "左ウィンドウへ" })
 map("n", "<C-j>", "<C-w>j", { desc = "下ウィンドウへ" })
 map("n", "<C-k>", "<C-w>k", { desc = "上ウィンドウへ" })
 map("n", "<C-l>", "<C-w>l", { desc = "右ウィンドウへ" })
+
